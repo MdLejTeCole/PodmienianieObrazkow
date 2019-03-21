ea473b42e430325a945290726e186b99c5383984
MD <m.rybak996@gmail.com>
2019-03-21 14:56:21 +0100

    Wersja dzialajaca

--- PodmianaObrazka/PodmianaObrazka/Page2.xaml.cs
+++ PodmianaObrazka/PodmianaObrazka/Page2.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -9,51 +10,210 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace PodmianaObrazka
+
 {
-    public class Page2ViewModel
+    public class Page2ViewModel : INotifyPropertyChanged
     {
-        public ImageSource MyProperty1 { get; set; }
-        public ImageSource MyProperty2 { get; set; }
-        public ImageSource MyProperty3 { get; set; }
-        public ImageSource MyProperty4 { get; set; }
-        public ImageSource MyProperty5 { get; set; }
-        public ImageSource MyProperty6 { get; set; }
-
-        
-    }
-    [ContentProperty(nameof(Source))]
-    public class ImageResourceExtension : IMarkupExtension
-    {
-        public string Source { get; set; }
-
-        public object ProvideValue(IServiceProvider serviceProvider) 
+        private ImageSource _myProperty1;
+        public ImageSource MyProperty1
         {
-            if (Source == null)
+            get { return _myProperty1; }
+            set
             {
-                return null;
+                _myProperty1 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty1"));
             }
+        }
+        private ImageSource _myProperty2;
+        public ImageSource MyProperty2
+        {
+            get { return _myProperty2; }
+            set
+            {
+                _myProperty2 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty2"));
+            }
+        }
+        private ImageSource _myProperty3;
+        public ImageSource MyProperty3
+        {
+            get { return _myProperty3; }
+            set
+            {
+                _myProperty3 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty3"));
+            }
+        }
+        private ImageSource _myProperty4;
+        public ImageSource MyProperty4
+        {
+            get { return _myProperty4; }
+            set
+            {
+                _myProperty4 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty4"));
+            }
+        }
+        private ImageSource _myProperty5;
+        public ImageSource MyProperty5
+        {
+            get { return _myProperty5; }
+            set
+            {
+                _myProperty5 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty5"));
+            }
+        }
+        private ImageSource _myProperty6;
+        public ImageSource MyProperty6
+        {
+            get { return _myProperty6; }
+            set
+            {
+                _myProperty6 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty6"));
+            }
+        }
+        private ImageSource _myProperty7;
+        public ImageSource MyProperty7
+        {
+            get { return _myProperty7; }
+            set
+            {
+                _myProperty7 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty7"));
+            }
+        }
+        private ImageSource _myProperty8;
+        public ImageSource MyProperty8
+        {
+            get { return _myProperty8; }
+            set
+            {
+                _myProperty8 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty8"));
+            }
+        }
+        private ImageSource _myProperty9;
+        public ImageSource MyProperty9
+        {
+            get { return _myProperty9; }
+            set
+            {
+                _myProperty9 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty9"));
+            }
+        }
+        private ImageSource _myProperty10;
+        public ImageSource MyProperty10
+        {
+            get { return _myProperty10; }
+            set
+            {
+                _myProperty10 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty10"));
+            }
+        }
+        private ImageSource _myProperty11;
+        public ImageSource MyProperty11
+        {
+            get { return _myProperty11; }
+            set
+            {
+                _myProperty11 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty11"));
+            }
+        }
+        private ImageSource _myProperty12;
+        public ImageSource MyProperty12
+        {
+            get { return _myProperty12; }
+            set
+            {
+                _myProperty12 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty12"));
+            }
+        }
+        private ImageSource _myProperty13;
+        public ImageSource MyProperty13
+        {
+            get { return _myProperty13; }
+            set
+            {
+                _myProperty13 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty13"));
+            }
+        }
+        private ImageSource _myProperty14;
+        public ImageSource MyProperty14
+        {
+            get { return _myProperty14; }
+            set
+            {
+                _myProperty14 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty14"));
+            }
+        }
+        private ImageSource _myProperty15;
+        public ImageSource MyProperty15
+        {
+            get { return _myProperty15; }
+            set
+            {
+                _myProperty15 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty15"));
+            }
+        }
+        private ImageSource _myProperty16;
+        public ImageSource MyProperty16
+        {
+            get { return _myProperty16; }
+            set
+            {
+                _myProperty16 = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MyProperty16"));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
 
-            // Do your translation lookup here, using whatever method you require
-            var imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
-
-            return imageSource;
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
         }
     }
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Page2 : ContentPage
-	{
+    public partial class Page2 : ContentPage
+    {
         String[] zrodla = new String[16];
         ImageButton[] nazwy = new ImageButton[16];
         int[] licz = new int[16];
         int a = 0;
         int b = 0;
         bool pierwszy = true;
-        ImageResourceExtension odswiezObrazki = new ImageResourceExtension();
+        //ImageResourceExtension odswiezObrazki = new ImageResourceExtension();
         Page2ViewModel viewModel = new Page2ViewModel();
         public Page2()
         {
             BindingContext = viewModel;
             InitializeComponent();
+
+            viewModel.MyProperty1 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o1.png", typeof(Page2).GetTypeInfo().Assembly);
+            viewModel.MyProperty2 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o2.png", typeof(Page2).GetTypeInfo().Assembly);
+            viewModel.MyProperty3 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o3.png", typeof(Page2).GetTypeInfo().Assembly);
+            viewModel.MyProperty4 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o4.png", typeof(Page2).GetTypeInfo().Assembly);
+            viewModel.MyProperty5 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o5.png", typeof(Page2).GetTypeInfo().Assembly);
+            viewModel.MyProperty6 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o6.png", typeof(Page2).GetTypeInfo().Assembly);
+            viewModel.MyProperty7 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o7.png", typeof(Page2).GetTypeInfo().Assembly);
+            viewModel.MyProperty8 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o8.png", typeof(Page2).GetTypeInfo().Assembly);
+            viewModel.MyProperty9 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o9.png", typeof(Page2).GetTypeInfo().Assembly);
+            viewModel.MyProperty10 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o10.png", typeof(Page2).GetTypeInfo().Assembly);
+            viewModel.MyProperty11 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o11.png", typeof(Page2).GetTypeInfo().Assembly);
+            viewModel.MyProperty12 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o12.png", typeof(Page2).GetTypeInfo().Assembly);
+            viewModel.MyProperty13 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o13.png", typeof(Page2).GetTypeInfo().Assembly);
+            viewModel.MyProperty14 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o14.png", typeof(Page2).GetTypeInfo().Assembly);
+            viewModel.MyProperty15 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o15.png", typeof(Page2).GetTypeInfo().Assembly);
+            viewModel.MyProperty16 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o16.png", typeof(Page2).GetTypeInfo().Assembly);
 
             nazwy[0] = przycisk1;
             nazwy[1] = przycisk2;
@@ -71,25 +231,23 @@ namespace PodmianaObrazka
             nazwy[13] = przycisk14;
             nazwy[14] = przycisk15;
             nazwy[15] = przycisk16;
-           viewModel.MyProperty1 = ImageSource.FromResource("{local:ImageResource PodmianaObrazka.Obrazki.o3.png}", typeof(Page2).GetTypeInfo().Assembly);
-            //viewModel.MyProperty1 = ImageSource.FromResource("PodmianaObrazka.Obrazki.o1.png");
-            //viewModel.MyProperty1 = "PodmianaObrazka.Obrazki.o1.png";
-            viewModel.MyProperty2 = "{PodmianaObrazka.Obrazki.o2.png}";
-            viewModel.MyProperty3 = "{local:ImageResource PodmianaObrazka.Obrazki.o3.png}";
-            viewModel.MyProperty4 = "{local:ImageResource PodmianaObrazka.Obrazki.o4.png}";
-            viewModel.MyProperty5 = "{local:ImageResource PodmianaObrazka.Obrazki.o5.png}";
-            viewModel.MyProperty6 = "{local:ImageResource PodmianaObrazka.Obrazki.o6.png}";
-            var test = przycisk10.Source; 
-            zrodla[6] = "{local:ImageResource PodmianaObrazka.Obrazki.o7.png}";
-            zrodla[7] = "{local:ImageResource PodmianaObrazka.Obrazki.o8.png}";
-            zrodla[8] = "{local:ImageResource PodmianaObrazka.Obrazki.o9.png}";
-            zrodla[9] = "{local:ImageResource PodmianaObrazka.Obrazki.o10.png}";
-            zrodla[10] = "{local:ImageResource PodmianaObrazka.Obrazki.o11.png}";
-            zrodla[11] = "{local:ImageResource PodmianaObrazka.Obrazki.o12.png}";
-            zrodla[12] = "{local:ImageResource PodmianaObrazka.Obrazki.o13.png}";
-            zrodla[13] = "{local:ImageResource PodmianaObrazka.Obrazki.o14.png}";
-            zrodla[14] = "{local:ImageResource PodmianaObrazka.Obrazki.o15.png}";
-            zrodla[15] = "{local:ImageResource PodmianaObrazka.Obrazki.o16.png}";
+
+            zrodla[0] = "PodmianaObrazka.Obrazki.o1.png";
+            zrodla[1] = "PodmianaObrazka.Obrazki.o2.png";
+            zrodla[2] = "PodmianaObrazka.Obrazki.o3.png";
+            zrodla[3] = "PodmianaObrazka.Obrazki.o4.png";
+            zrodla[4] = "PodmianaObrazka.Obrazki.o5.png";
+            zrodla[5] = "PodmianaObrazka.Obrazki.o6.png";
+            zrodla[6] = "PodmianaObrazka.Obrazki.o7.png";
+            zrodla[7] = "PodmianaObrazka.Obrazki.o8.png";
+            zrodla[8] = "PodmianaObrazka.Obrazki.o9.png";
+            zrodla[9] = "PodmianaObrazka.Obrazki.o10.png";
+            zrodla[10] = "PodmianaObrazka.Obrazki.o11.png";
+            zrodla[11] = "PodmianaObrazka.Obrazki.o12.png";
+            zrodla[12] = "PodmianaObrazka.Obrazki.o13.png";
+            zrodla[13] = "PodmianaObrazka.Obrazki.o14.png";
+            zrodla[14] = "PodmianaObrazka.Obrazki.o15.png";
+            zrodla[15] = "PodmianaObrazka.Obrazki.o16.png";
             Wymieszaj();
         }
         private void Wymieszaj()
@@ -128,11 +286,17 @@ namespace PodmianaObrazka
                     }
                 }
             }
+            PropertyInfo[] myPropertyInfo;
+
+            myPropertyInfo = Type.GetType("PodmianaObrazka.Page2ViewModel").GetProperties();
+            object value = myPropertyInfo[a].GetValue(viewModel, null);
+            
 
             for (int i = 0; i < 16; i++)
             {
-                //nazwy[i].Source = zrodla[licz[i]];
-            }           
+                
+                myPropertyInfo[i].SetValue(viewModel, ImageSource.FromResource(zrodla[licz[i]], typeof(Page2).GetTypeInfo().Assembly));
+            }
         }
         private async void Sprawdz()
         {
@@ -172,7 +336,7 @@ namespace PodmianaObrazka
         private void Przycisk1_Clicked(object sender, EventArgs e)
         {
             ImageButton p = sender as ImageButton;
-            
+
             if (pierwszy == true)
             {
                 for (int i = 0; i < 16; i++)
@@ -193,10 +357,13 @@ namespace PodmianaObrazka
                         b = i;
                     }
                 }
-                nazwy[a].Source = zrodla[licz[b]];
-                nazwy[b].Source = zrodla[licz[a]];
-                viewModel.MyProperty1 = zrodla[licz[b]];
-                viewModel.MyProperty2 = zrodla[licz[a]];
+
+                PropertyInfo[] myPropertyInfo;
+
+                myPropertyInfo = Type.GetType("PodmianaObrazka.Page2ViewModel").GetProperties();
+                object value = myPropertyInfo[a].GetValue(viewModel, null);
+                myPropertyInfo[a].SetValue(viewModel, ImageSource.FromResource(zrodla[licz[b]], typeof(Page2).GetTypeInfo().Assembly));
+                myPropertyInfo[b].SetValue(viewModel, ImageSource.FromResource(zrodla[licz[a]], typeof(Page2).GetTypeInfo().Assembly));
                 int c = licz[a];
                 licz[a] = licz[b];
                 licz[b] = c;
